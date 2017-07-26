cf7995328b295d66abd7f8cf9c9f5a11114c7ce7
root <doyoofficial@gmail.com>
2017-07-26 18:34:13 +0700

    aplikasi crud

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+  </head>
+
+  <body>
+    <header>
+      <h3>Pendaftaran Siswa baru</h3>
+      <h1>SMK Coding</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+      <?php if(isset($_GET['status'])): ?>
+	    <p>
+		    <?php
+			  if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			   } else {
+				echo "Pendaftaran gagal!";}
+		    ?>
+	    </p>
+      <?php endif; ?>
+      <ul>
+        <li><a href="form-daftar.php">Daftar Baru</a></li>
+        <li><a href="list-siswa.php">Pendaftaran</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
